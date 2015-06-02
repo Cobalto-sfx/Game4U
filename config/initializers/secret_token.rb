84306215def4793bbfecffc03396350e8496fa28
Drulas <javier_druet_93@hotmail.com>
2015-06-02 20:24:16 +0200

    final commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Rails4TwitterClone::Application.config.secret_key_base = '919b903490e011c8b2a6b7566dbcd40cf720d9bab7b7255b32e528337bb1483aa00f4238c133bb2e23050dc22d04923b018c4256171ab7cd46369777f79a0cb2'
+Game4U::Application.config.secret_key_base = '919b903490e011c8b2a6b7566dbcd40cf720d9bab7b7255b32e528337bb1483aa00f4238c133bb2e23050dc22d04923b018c4256171ab7cd46369777f79a0cb2'
